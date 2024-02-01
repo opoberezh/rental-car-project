0a905e08f7ff6728749ae1c0d13a198f848e022e
opoberezh <124092276+opoberezh@users.noreply.github.com>
2024-02-01 15:07:48 +0000

    Deploying to gh-pages from @ opoberezh/rental-car-project@e8aaa0c9a99eeecae659729fb99c85ae35dabcbb 🚀

---
+++ assets/Favorite-9b8051d1.js
@@ -0,0 +1,3 @@
+import{n as t,j as e}from"./index-a7ccdd91.js";const o=t.h1`
+  color: #000000;
+`,s=()=>e.jsx(o,{children:"Half Page"});export{s as default};
---
+++ assets/Home-ccaa77c4.js
@@ -0,0 +1,75 @@
+import{n as e,L as i,j as t}from"./index-a7ccdd91.js";const n="/react_vite/assets/bmwHero-30951bd9.jpg",o=e.div`
+
+  background-image: url(${n});
+  background-repeat: no-repeat;
+  background-position: left top;
+  background-size: 100% 100%;
+`,p=e.div`
+  display: flex;
+  
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  width: 375px;
+  height: 995px;
+  padding: 100px 20px 0 20px;
+  @media (min-width: 1440px){
+    width: 425px;
+   
+  }
+`,a=e.h1`
+  margin: 0;
+  font-size: 28px;
+  font-weight: 700;
+  line-height: 1.17;
+  text-align: center;
+  color: #ffffff;
+  @media (min-width: 768px){
+    font-size: 32px;
+  }
+  @media (min-width: 1440px){
+    font-size: 38px;
+  }
+`,r=e(i)`
+  display: flex;
+  width: 140px;
+  height: 4px;
+  padding: 24px 10px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  background: #3470ff;
+  color: #fff;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  font-family: 'Manrope';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.42;
+  text-transform: uppercase;
+  text-decoration: none;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  &:hover {
+    background-color: #0b44cd;
+  }
+`,x=e.span`
+font-family: 'Rubik Glitch Pop', system-ui;
+font-size: 32px;
+color: #000000;
+text-shadow:
+    0 0 5px #3470ff,
+    0 0 10px #3470ff,
+    0 0 20px #ffffff,
+    0 0 40px #00baff,
+    0 0 80px #00baff;
+    @media (min-width: 768px){
+      font-size: 38px;
+    }
+    @media (min-width: 1440px){
+      font-size: 42px;
+    }
+`,f=()=>t.jsx(o,{children:t.jsxs(p,{children:[t.jsxs(a,{children:["Take it easy with",t.jsx(x,{children:" carFORrent"})," Enjoy your trip"]}),t.jsx(r,{to:"/catalog",children:"Let's start"})]})});export{f as default};
