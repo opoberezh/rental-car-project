73d78e667920eacc86eccb8df69f40f5b4c9457c
opoberezh <124092276+opoberezh@users.noreply.github.com>
2024-02-01 19:44:59 +0000

    Deploying to gh-pages from @ opoberezh/rental-car-project@4255acf7dfd494951abc6760bfed2937806a84cb 🚀

--- assets/ErrorPage-4b8ec1c6.js
+++ assets/ErrorPage-4b8ec1c6.js
@@ -1,4 +1,4 @@
-import{n as f,j as t}from"./index-9eb1936d.js";const e=f.div`
+import{n as f,j as t}from"./index-adecb89f.js";const e=f.div`
   display: flex;
   justify-content: center;
   align-items: center;
--- assets/Favorite-2a86cbce.js
+++ assets/Favorite-2a86cbce.js
@@ -1,3 +1,3 @@
-import{n as t,j as e}from"./index-9eb1936d.js";const o=t.h1`
+import{n as t,j as e}from"./index-adecb89f.js";const o=t.h1`
   color: #000000;
 `,s=()=>e.jsx(o,{children:"Half Page"});export{s as default};
